fd0de0a2efc6ff7fc8aa6fbd37d2603edf745aba
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-04-22 21:05:02 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -10,7 +10,7 @@ public double getSlope (){
   return -(double)a/b;
 }
 public boolean isOnLine (int a2, int b2){
-  if (a*a2 + b*b2 + c1 == 0){
+  if (a*a2 + b*b2 + c == 0){
     return true;
   }
   return false;
